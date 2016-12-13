ee8e682ac6483e8f8fedae4067b6d6e9db7291b9
Harshil <harshilyadav@rocketmail.com>
2016-12-13 23:37:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/harshil/newsinsights/ArticleActivity.java
@@ -0,0 +1,38 @@
+package com.example.harshil.newsinsights;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.SpinnerAdapter;
+
+public class ArticleActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        Intent i = getIntent();
+
+        String url = i.getStringExtra("articleUrl");
+        String content = i.getStringExtra("content");
+
+        webView.loadUrl(url);
+        //webView.loadData(content, "text/html", "UTF-8");
+
+    }
+
+
+}
